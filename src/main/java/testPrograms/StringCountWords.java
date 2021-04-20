3821e1211cae440786cd715574c405deb80467a8
vshrimangale <vshrimangale@li372-244.members.linode.com>
2021-04-20 09:11:19 -0400

    commiting test to Branch X

---
+++ src/main/java/testPrograms/StringCountWords.java
@@ -0,0 +1,44 @@
+package testPrograms;
+
+import java.util.HashMap;
+
+import org.apache.commons.collections4.map.HashedMap;
+
+public class StringCountWords {
+
+
+	public static void main(String[] args) {
+
+		String str="This is vijay vijay and shri and shri";
+		String[] split=str.split(" ");
+
+		HashMap<String,Integer> map=new HashMap<String, Integer>();
+
+		for(int i=0;i<split.length;i++)
+		{
+			if(map.containsKey(split[i])) 
+			{
+				int count=map.get(split[i]);
+				map.put(split[i],count+1);
+			}
+
+			else
+			{
+				map.put(split[i],1);
+			}
+
+			
+		}
+
+		System.out.println(map);
+
+
+
+
+
+
+
+	}
+
+
+}
